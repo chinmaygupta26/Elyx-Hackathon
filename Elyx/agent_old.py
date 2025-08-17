559b0f2ca9d1b2edcdf0e718f56aea676e7c890d
chinmaygupta26 <103333796+chinmaygupta26@users.noreply.github.com>
2025-08-18 00:00:53 +0530

    Update agent_old.py

--- Elyx/agent_old.py
+++ Elyx/agent_old.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDxJOx6DS-WUBhXiiskYos3HkTyqUEpkZw"
+os.environ["GOOGLE_API_KEY"] = "ENTER_YOUR_GEMINI_API_KEY"
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 
